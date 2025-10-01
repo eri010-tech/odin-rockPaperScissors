e236f012d6c474e9d0600a8363836f0b06825745
Mya <211755169+eri010-tech@users.noreply.github.com>
2025-09-30 19:29:38 -0700

    edits to javascript

---
+++ index.js
@@ -0,0 +1,11 @@
+/* write a function that randonly returns
+"rock", "paper", or "scissors". */
+
+function getComputerChoice(computerChoice){
+  if (Math.random() < 1/3) {
+    
+  }
+}
+console.log(getComputerChoice("computerChoice"));
+console.log(typeof "Rock")
+console.log(typeof Math.random())
