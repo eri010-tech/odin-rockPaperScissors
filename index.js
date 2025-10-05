f054a585549810488a0b52dec53444e9c0c05748
Mya <211755169+eri010-tech@users.noreply.github.com>
2025-10-05 11:12:28 -0700

    final edits for the morning

--- index.js
+++ index.js
@@ -7,13 +7,29 @@ function getComputerChoice() {
     getComputerChoice = 'scissors';
   } else if (randomNumber >= 1/3 && randomNumber < 2/3) {
     getComputerChoice = 'rock';
-  } else if (randomNumber >= 2/3 && randomNumber < 1)
+  } else if (randomNumber >= 2/3 && randomNumber < 1) {
     getComputerChoice = 'paper';
+  }
   
   return getComputerChoice;
 }
 console.log(getComputerChoice());
 
+/* write the logic to get the human choice.
+Write a function that takes the user choice 
+and returns it */
+function getHumanChoice (){
+  let humanChoice = window.prompt('please pick: rock, paper, or scissors');
 
-   
+  if (humanChoice === 'rock') {
+    getHumanChoice = 'rock';
+  } else if (humanChoice === 'paper') {
+    getHumanChoice = 'paper';
+  } else if (humanChoice === 'scissors') {
+    getHumanChoice = 'scissors';
+  }
+  return getHumanChoice;
+}
+console.log(getHumanChoice());
+    
 
