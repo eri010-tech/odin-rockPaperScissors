219f5990161061a5843af2dc86c4bec7cc10d096
Mya <211755169+eri010-tech@users.noreply.github.com>
2025-10-06 22:02:03 -0700

    last edits for the night

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
 /* write a function that randonly returns
 "rock", "paper", or "scissors". */
+
 function getComputerChoice() {
   const randomNumber = Math.random();
+  let computerMove = ' ';
   
   if (randomNumber >= 0 && randomNumber < 1/3) {
-    getComputerChoice = 'scissors';
+    computerMove = 'scissors';
   } else if (randomNumber >= 1/3 && randomNumber < 2/3) {
-    getComputerChoice = 'rock';
+    computerMove = 'rock';
   } else if (randomNumber >= 2/3 && randomNumber < 1) {
-    getComputerChoice = 'paper';
+    computerMove = 'paper';
   }
-  return getComputerChoice;
+  return computerMove;
 }
-console.log(getComputerChoice());
+console.log(getComputerChoice( ));
 
 /* write the logic to get the human choice.
 Write a function that takes the user choice 
 and returns it */
+
 function getHumanChoice () {
   let humanChoice = window.prompt('please pick: rock, paper, or scissors');
+  let userChoice = ' ';
 
   if (humanChoice === 'rock') {
-    getHumanChoice = 'rock';
+    userChoice = 'rock';
   } else if (humanChoice === 'paper') {
-    getHumanChoice = 'paper';
+    userChoice = 'paper';
   } else if (humanChoice === 'scissors') {
-    getHumanChoice = 'scissors';
+    userChoice = 'scissors';
   }
-  return getHumanChoice;
+   return userChoice;
 }
+/* console.log(typeof getHumanChoice()); This helped me discovery that my getHumanChoice 
+function is being treated as a string */
+
 console.log(getHumanChoice());
 
 let humanScore = 0;
@@ -41,24 +48,13 @@ that win and the signs that loose. maybe try nesting */
 //increment the score based on the winner. might try using a loop for 5 rounds 
 
 function playRound (humanChoice, computerChoice){
-  if (humanChoice === 'paper' && computerChoice === 'rock') {
-    console.log("You win! Paper beats Rock.");
-  } else if (humanChoice === 'scissors' && computerChoice === 'paper'){
-    console.log("You win! Scissors beats Paper.");
-  } else if(humanChoice === 'rock' && computerChoice === 'scissors'){
-    console.log("You win! Rock beats Scissors.")
-  } else if (humanChoice === 'rock' && computerChoice === 'rock'){
-    console.log("Tie!");
-  } else if(humanChoice === 'paper' && computerChoice === 'paper'){
-    console.log("Tie!");
-  } else if (humanChoice === 'scissors' && computerChoice === 'scissors'){
-    console.log("Tie!")
-  } 
-}
+  
+} 
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+
+
+
 /* current issue: getHumanChoice and getComputerChoice are receiving a type error:
 "not a function".this is likely due to scope. I will need to resolve it */
 
-console.log(playRound(humanSelection, computerSelection));
+
