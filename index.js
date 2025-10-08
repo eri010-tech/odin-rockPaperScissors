5639317d5716fb84fea1bb5c629cfc538ef4fc86
Mya <211755169+eri010-tech@users.noreply.github.com>
2025-10-08 10:30:56 -0700

    sick

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ function getComputerChoice() {
   let computerMove = ' ';
   
   if (randomNumber >= 0 && randomNumber < 1/3) {
-    computerMove = 'scissors';
-  } else if (randomNumber >= 1/3 && randomNumber < 2/3) {
     computerMove = 'rock';
-  } else if (randomNumber >= 2/3 && randomNumber < 1) {
+  } else if (randomNumber >= 1/3 && randomNumber < 2/3) {
     computerMove = 'paper';
+  } else if (randomNumber >= 2/3 && randomNumber < 1) {
+    computerMove = 'scissors';
   }
   return computerMove;
 }
@@ -20,15 +20,16 @@ console.log(getComputerChoice( ));
 Write a function that takes the user choice 
 and returns it */
 
+let humanOption = window.prompt('please pick: rock, paper, or scissors');
+
 function getHumanChoice () {
-  let humanChoice = window.prompt('please pick: rock, paper, or scissors');
   let userChoice = ' ';
 
-  if (humanChoice === 'rock') {
+  if (humanOption === 'rock') {
     userChoice = 'rock';
-  } else if (humanChoice === 'paper') {
+  } else if (humanOption === 'paper') {
     userChoice = 'paper';
-  } else if (humanChoice === 'scissors') {
+  } else if (humanOption === 'scissors') {
     userChoice = 'scissors';
   }
    return userChoice;
@@ -38,8 +39,8 @@ function is being treated as a string */
 
 console.log(getHumanChoice());
 
-let humanScore = 0;
-let computerScore = 0;
+//let humanScore = 0;
+//let computerScore = 0;
 
 //step 1: create the logic for who wins and looses  
 /* try to condense the code because you need two perspectives: the signs 
@@ -48,10 +49,43 @@ that win and the signs that loose. maybe try nesting */
 //increment the score based on the winner. might try using a loop for 5 rounds 
 
 function playRound (humanChoice, computerChoice){
-  
+  if(humanChoice === 'rock'){
+    if(computerChoice === 'rock'){
+      result = 'Tie.';
+    } else if (computerChoice === 'paper'){
+      result = 'You lose.';
+    } else if (computerChoice === 'scissors'){
+      result = 'You Win!';
+    }
+  } else if (humanChoice === 'paper'){
+    if(computerChoice === 'rock'){
+      result = 'You Win!';
+    } else if (computerChoice === 'paper'){
+      result = 'Tie.';
+    } else if (computerChoice === 'scissors'){
+      result = 'You lose.';
+    }
+  } else if (humanChoice === 'scissor'){
+    if (computerChoice === 'rock'){
+      result = 'You lose.';
+    } else if (computerChoice === 'paper'){
+      result = 'You Win!';
+    } else if (computerChoice === 'scissors'){
+      result = 'Tie.';
+    }
+  }
 } 
 
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
+playRound(humanSelection, computerSelection);
+
+console.log(typeof getHumanChoice());
+console.log(typeof getComputerChoice());
+//my functions are currently strings :( 
+/* current problem: 
+I need to figure out a way to keep my functions as functions and not strings */
 
 
 /* current issue: getHumanChoice and getComputerChoice are receiving a type error:
