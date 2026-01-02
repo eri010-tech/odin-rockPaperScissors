d547d89ead2ea4a8f1725b3eb7a752ad1309b10b
Mya <211755169+eri010-tech@users.noreply.github.com>
2026-01-01 22:41:21 -0800

    almost done

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
+// ####### revisiting project ######### //
+
+// creating variables for each button
+const buttons = document.querySelectorAll("button"); 
+const btnContainer = document.querySelectorAll("#btn-container");
+const userPicked = document.querySelector("#user-picked");
+const computerPicked = document.querySelector("#computer-picked");
+const scoreContainer = document.querySelector("#score-container")
+const winner = document.querySelector("#winner"); 
+let yourScore = document.querySelector("#player-score"); 
+let opponentScore = document.querySelector("#computer-score");
+
+
+// loop through the btnContainer and attach an eventListener to each button
+
+btnContainer.forEach((button) => {
+  button.addEventListener('click', (e) => {
+
+     let humanChoice = e.target.textContent; 
+     userPicked.textContent = humanChoice; 
+    
+    let computerMove = computerChoice(); 
+    computerPicked.textContent = computerMove; 
+
+
+    let roundWinner = playRound(humanChoice, computerMove); 
+   
+    updateScore(roundWinner); 
+
+   
+     
+   //next I need make it so that a game end after one person 
+   // gets 5 points. After, maybe create a div that says: game over.
+   // then I want the score board to restart over. 
+  });
+});
+
+function computerChoice() {
+    const randomNumber = Math.random();
+    let computerMove = ' ';
+  
+    if (randomNumber >= 0 && randomNumber < 1/3) {
+      computerMove = 'rock';
+    } else if (randomNumber >= 1/3 && randomNumber < 2/3) {
+      computerMove = 'paper';
+    } else if (randomNumber >= 2/3 && randomNumber < 1) {
+      computerMove = 'scissors';
+    }
+    return computerMove;
+};
+
 
 // Revised playRound using solution from Stack Overflow
 
@@ -20,72 +71,30 @@ function playRound (playerSelection, computerSelection){
   }
     
   return roundWinner; 
-}; 
-
-/*
-function playGame(){
-  for (let i = 0; i < 5; i++ ){
-    let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
-    console.log(playRound(humanSelection, computerSelection));
-    alert(`The computer picked: ${computerSelection}`);
-    console.log("Your score = " + userScore);
-    console.log("Computer's score = " + computerScore);
-  }
-  if (userScore > computerScore){
-    console.log(`You Won! Your final score was: ${userScore}/5`);
-  } else if(computerScore > userScore){
-    console.log(`You lost. Your final score was: ${userScore}/5. 
-    Better luck next time!`);
-  }
-}
-playGame();
-*/
-
-
-// ####### revisiting project ######### //
-
-// creating variables for each button
-const buttons = document.querySelectorAll("button"); 
-const btnContainer = document.querySelectorAll("#btn-container");
-const humanScoreDisplay = document.querySelector("#human-score");
-const userPicked = document.querySelector("#user-picked");
-const computerPicked = document.querySelector("#computer-picked");
-const computerScoreDisplay = document.querySelector("#computer-score");
-const winner = document.querySelector("#winner"); 
-
-
-// loop through the btnContainer and attach an eventListener to each button
-
-btnContainer.forEach((button) => {
-  button.addEventListener('click', (e) => {
-
-     let humanChoice = e.target.textContent; 
-     userPicked.textContent = humanChoice; 
-    
-    let computerMove = computerChoice(); 
-    computerPicked.textContent = computerMove; 
-
-
-   let roundWinner = playRound(humanChoice, computerMove); 
-   console.log(roundWinner); // says who won the round 
-   
-  });
-});
-
-function computerChoice() {
-    const randomNumber = Math.random();
-    let computerMove = ' ';
-  
-    if (randomNumber >= 0 && randomNumber < 1/3) {
-      computerMove = 'rock';
-    } else if (randomNumber >= 1/3 && randomNumber < 2/3) {
-      computerMove = 'paper';
-    } else if (randomNumber >= 2/3 && randomNumber < 1) {
-      computerMove = 'scissors';
-    }
-    return computerMove;
 };
+
+
+
+  let computerScore = 0; 
+  let humanScore = 0; 
+
+function updateScore (currentRoundWinner){
+  if(currentRoundWinner === "player"){
+    humanScore++; 
+  } else if (currentRoundWinner === "computer"){
+    computerScore++; 
+  } 
+  yourScore.textContent = humanScore;
+  opponentScore.textContent = computerScore; 
+};
+ 
+
+
+
+
+ 
+
+
 
 /* When a button is clicked, a comparison should be made 
 between the user's choice and the computer's choice.
